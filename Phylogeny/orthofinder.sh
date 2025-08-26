6a72fec3b723f1ccc3a7c99df18cc28e06dc03e2
Roberto Biello <r.s.biello@gmail.com>
2025-08-26 10:52:18 +0200

    Update orthofinder.sh

--- Phylogeny/orthofinder.sh
+++ Phylogeny/orthofinder.sh
@@ -1,1 +1,38 @@
+#!/bin/bash -e
+
+# Print basic job info
+pwd; hostname; date
+
+# ================================
+# Activate Conda Environment
+# ================================
+conda activate env
+
+# ================================
+# Define Input Variables
+# ================================
+
+# Working directory containing FASTA files
+# Each species should have one FASTA file with protein sequences
+WD=/path/to/orthofinder/input
+
+# Move to working directory
+cd $WD
+
+# ================================
+# Run OrthoFinder
+# ================================
+# -t : threads for sequence search
+# -a : threads for tree inference
+# -M : multiple sequence alignment method
+# -S : sequence search program (DIAMOND in ultra-sensitive mode)
+# -T : tree inference program (RAxML-NG)
+# -f : input folder with protein FASTA files
+orthofinder \
+  -t 8 \
+  -a 8 \
+  -M msa \
+  -S diamond_ultra_sens \
+  -T raxml-ng \
+  -f $WD
 
